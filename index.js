e478dd4b1e6af4960f504305280918864ef8518c
Lucian Parfeni <lucian.parfeni@cynny.com>
2015-02-13 00:00:46 +0200

    couchDB support with nano | functioning list add remove

--- index.js
+++ index.js
@@ -8,130 +8,142 @@ var listData = require('save')('lists');
 var port = 5454;
 
 
+var nano = require('nano')('http://localhost:5984');
+var cdb = nano.db;
 
-// var lists = {
-// 	'a1': vids
-// };
+var musicList;
 
-var vids = [
-	'A_q5bdrelvI',
-	'Jcu1AHaTchM',
-	'5OVvJOeUdUs',
-	'Zh3VENebCgk'
-];
-
-listData.create({
-	name: 'default',
-	vids: vids
-}, function(error, list) {
-	if (error) {
-		console.log('err creating list');
-	}
-});
-
-
-// http.createServer(function(request, response) {
-// 	response.writeHead(200, {
-// 		'Content-Type': 'text/plain'
-// 	});
-// 	response.write('Hello World\n');
-// 	response.end();
-// }).listen(port);
-
-
-var server = restify.createServer({
-	name: 'tuber'
-});
-server.listen(port, function() {
-	console.log(colors.green('listening on port ' + port));
-});
-
-server
-	.use(restify.fullResponse())
-	.use(restify.bodyParser());
-
-// server.get('/list', function(req, res, next) {
-// 	listData.find({}, function(err, list) {
-// 		res.send(list);
-// 		// res.send(JSON.stringify(vids));
-// 	});
-// });
-
-server.post('/list/create', function(req, res, next) {
-	console.log('\n called list/create');
-	console.log(req.params);
-
-
-	if (req.params.name === undefined) {
-		return next(new restify.InvalidArgumentError('Name must be supplied'));
-	}
-
-	listData.create({
-		name: req.params.name
-	}, function(error, list) {
-		if (error) {
-			return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+cdb.list(function(err, body) {
+	body.forEach(function(db) {
+		if (db === 'music') {
+			musicList = cdb.use('music');
 		}
-
-		res.send(201, list);
 	});
+
+	if (!musicList) {
+		cdb.create('music', function() {
+			cdb.use('music');
+		});
+	}
+
+	dbReady();
 });
 
-server.post('/list/get', function(req, res, next) {
-	console.log('\n called list/get');
-	console.log(req.params.name);
 
-	listData.findOne({
-		name: req.params.name
-	}, function(error, list) {
-		if (error) {
-			return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+function dbReady() {
+	var vids = [
+		'A_q5bdrelvI',
+		'Jcu1AHaTchM',
+		'5OVvJOeUdUs',
+		'Zh3VENebCgk'
+	];
+
+	var listData = {
+		list: vids
+	};
+
+	function update(callback) {
+		musicList.insert(listData, 'default', function(err, body) {
+			if (err) {
+				console.log('pula');
+				callback(err);
+				return;
+			}
+
+			callback && callback(null);
+		});
+	}
+
+	function getDefaultList(callback) {
+		musicList.get('default', {}, function(err, body) {
+			if (err) {
+				if (err.statusCode === 404) {
+					update(getDefaultList);
+					return;
+				}
+			}
+
+			listData = body;
+			callback && callback();
+		});
+	}
+
+	function addToList(vidID, callback) {
+		listData.list.push(vidID);
+		update(callback);
+	}
+
+	function removeFromList(vidID, callback) {
+		var index = listData.list.indexOf(vidID);
+		if (index !== -1) {
+			listData.list.splice(index, 1);
+			update(callback);
+		} else {
+			callback && callback('not found');
 		}
+	}
 
-		if (list) {
-			res.send(200, list);
+	getDefaultList();
+
+	/**
+	 *
+	 * server
+	 *
+	 */
+
+	var server = restify.createServer({
+		name: 'tuber'
+	});
+	server.listen(port, function() {
+		console.log(colors.green('listening on port ' + port + '\n'));
+	});
+
+	server
+		.use(restify.fullResponse())
+		.use(restify.bodyParser());
+
+
+	server.post('/list/get', function(req, res, next) {
+		console.log('get list', listData.list);
+
+		if (listData && listData.list) {
+			res.send(200, {
+				success: true,
+				name: listData._id,
+				list: listData.list
+			});
 		} else {
 			res.send(404);
 		}
 	});
-});
 
-server.post('/list/add', function(req, res, next) {
-	console.log('\n called list/add');
-	console.log(req.params);
+	server.post('/list/add', function(req, res, next) {
+		console.log('add list', req.params.id);
 
-	if (req.params.name === undefined || req.params.id === undefined) {
-		return next(new restify.InvalidArgumentError('Id must be supplied'));
-	}
-
-	listData.findOne({
-		name: req.params.name
-	}, function(error, list) {
-		if (error) {
-			return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
-		}
-
-		if (!list) {
-			var vids = [req.params.id];
-			listData.create({
-				name: req.params.name,
-				vids: vids
-			}, function(error, list) {
-				if (error) {
-					return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
-				}
-
-				res.send(201, list);
-			});
-		} else {
-			list.vids.push(req.params.id);
-
-			listData.update(list, function(error, list) {
-				if (error) {
-					return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
-				}
-				res.send(201, list);
+		if (listData && listData.list && req.params.id) {
+			addToList(req.params.id, function(err) {
+					res.send(201, {
+					success: true,
+					name: listData._id,
+					list: listData.list,
+					added: req.params.id
+				});
 			});
 		}
 	});
-});
+
+	server.post('/list/drop', function(req, res, next) {
+		console.log('drop list', req.params.id);
+
+		if (listData && listData.list && req.params.id) {
+			removeFromList(req.params.id, function(err) {
+				res.send(201, {
+					success: true,
+					name: listData._id,
+					list: listData.list,
+					added: req.params.id
+				});
+			});
+		}
+	});
+}
